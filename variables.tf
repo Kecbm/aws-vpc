9544c1b8b527fdcb24eeea1e03eabdc0b54c0126
Klecianny Melo <kleciannymelo@gmail.com>
2024-08-14 22:52:43 -0300

    :tada: iniciando o projeto de VPC definindo as variables

---
+++ variables.tf
@@ -0,0 +1,7 @@
+### GENERAL CONFIGS ###
+
+# Nome do projet. Essa variável será um prefixo para os recursos criados dentro desse projeto
+variable "project_name" {}
+
+# Região da AWS onde os recursos serão criados
+variable "region" {}
