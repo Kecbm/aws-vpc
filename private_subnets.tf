46762aaf485809a7510059e6c191e20648ed45fd
Klecianny Melo <kleciannymelo@gmail.com>
2024-08-19 21:12:25 -0300

    :bug: ajustando o nome do vpc_id

--- private_subnets.tf
+++ private_subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "private_subnet_1a" {
-  vpc_id            = awc_vpc.main.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/20"
   availability_zone = format("%sa", var.region)
 
@@ -9,7 +9,7 @@ resource "aws_subnet" "private_subnet_1a" {
 }
 
 resource "aws_subnet" "private_subnet_1b" {
-  vpc_id            = awc_vpc.main.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.16.0/20"
   availability_zone = format("%sb", var.region)
 
@@ -19,7 +19,7 @@ resource "aws_subnet" "private_subnet_1b" {
 }
 
 resource "aws_subnet" "private_subnet_1c" {
-  vpc_id            = awc_vpc.main.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.32.0/20"
   availability_zone = format("%sc", var.region)
 
@@ -27,5 +27,3 @@ resource "aws_subnet" "private_subnet_1c" {
     Name = format("%s-public-subnet-1c", var.project_name)
   }
 }
-
-# PAREI EM: 1. Day-1 - Aula 2 - 14:51 min
